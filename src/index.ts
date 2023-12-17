96a2aa473eaf0d84fc3c1967edd66ece7cb046c8
kyleboyer <kyleboyer@comcast.net>
2023-12-16 22:41:41 -0600

    Use commander

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,30 @@
 import { generateVideo } from './generator';
 import { loadConfig } from './utils/config';
+import { Command } from 'commander';
+const program = new Command();
 
-const config = loadConfig();
+program
+    .name('nodejs-tiktok-video-maker')
+    .description('Create Tiktok Videos with just ✨ one command ✨');
+// .version('0.0.1');
 
-generateVideo(config);
+program
+    .command('server')
+    .description('Start the GUI')
+    .option('-p, --port <number>', 'Port number to start the server on', parseInt, 8080)
+    .action((...args) => {
+      console.log(args);
+    });
+
+program
+    .command('generate', { isDefault: true })
+    .description('Run video generation once')
+    .option('-c, --config <file>', 'JSON config file to use', `${process.cwd()}/config.json`)
+    .action((options) => {
+      const config = loadConfig(options.config);
+      return generateVideo(config);
+    });
+
+
+program.parse();
+
