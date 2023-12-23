53364bc22746c22b0d3f813078f834b525cb4f1b
kyleboyer <kyleboyer@comcast.net>
2023-12-23 00:58:29 -0600

    More steps finished

--- src/gui/routes.ts
+++ src/gui/routes.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import { create } from 'express-handlebars';
-import { join } from 'path';
+import { join, parse } from 'path';
+import { listFiles } from '../utils/fs';
+import { fontsDir } from '../utils/dirs';
 export const app = express();
 const hbs = create({
   extname: '.hbs',
   helpers: {
-    now() {
-      return Date.now();
+    stringify: (obj: unknown) => JSON.stringify(obj),
+    foreach(iterableObj: Array<unknown>, block: { fn: CallableFunction }) {
+      let accum = '';
+      for (const item of iterableObj) {
+        accum += block.fn(item);
+      }
+      return accum;
     },
   },
 });
@@ -16,9 +23,14 @@ app.set('view engine', '.hbs');
 app.set('views', join(__dirname, 'views'));
 
 app.use(express.static(join(__dirname, 'public')));
+app.use('fonts', express.static(fontsDir));
 
 app.all(['/', '/index.html', 'index.htm'], (_req, res) => {
+  const fonts =
+    listFiles(fontsDir)
+        .filter((f)=>f.toLowerCase().endsWith('.ttf'))
+        .map((ttfFile) => parse(ttfFile).name);
   res.render('index.hbs', {
-    server_text: 'Hello from SS!',
+    fonts,
   });
 });
