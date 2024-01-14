ac113516ea135a4e15855d7159480b88f6ca88d4
kyleboyer <kyleboyer@comcast.net>
2024-01-13 22:29:04 -0600

    Logging

--- src/gui/routes.ts
+++ src/gui/routes.ts
@@ -99,7 +99,7 @@ export function getRoutes(config = {}) {
       });
       let lastData: string | Buffer;
       ptyProcess.onData((data) => {
-        console.log(data);
+        console.log(data.toString());
         res.write(data);
         lastData = data;
       });
